6996840e793c1566fa25ef5b2be77dc34d1e411a
Jonathan Compton <jonathankcompton@gmail.com>
2018-05-26 12:43:18 -0700

    Initial Commit

---
+++ Task-Buddy-Final-Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task-Buddy-Final-Project
+//
+//  Created by Jonathan Compton on 5/26/18.
+//  Copyright © 2018 Jonathan Compton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
